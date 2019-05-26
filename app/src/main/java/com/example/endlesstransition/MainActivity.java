799dee72308bd14ac410742609cce473aa707adf
maxumtkach <makstkach@bigmir.net>
2019-05-26 17:23:32 +0300

    //1.2.2 photo

--- app/src/main/java/com/example/endlesstransition/MainActivity.java
+++ app/src/main/java/com/example/endlesstransition/MainActivity.java
@@ -14,32 +14,30 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private TextView messagePhoto;
     Random random ;
 
     int min = 0;
-    int max = 100;
-    int number;
+      int max = 100;
+             int number;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+              setContentView(R.layout.activity_main);
 
          random = new Random(System.currentTimeMillis());  //инициализация переменных
-         number = min + random.nextInt(max - min + 1);
+              number = min + random.nextInt(max - min + 1);
 
         messagePhoto = findViewById(R.id.text_message_photo);
-        messagePhoto.setText(getString(R.string.photo) + number);
+               messagePhoto.setText(getString(R.string.photo) + number);
 
         Button clickStartButton = findViewById(R.id.button_start);
-        clickStartButton.setOnClickListener(clickStartListener);
+             clickStartButton.setOnClickListener(clickStartListener);
 
         Button clickEndButton = findViewById(R.id.button_end);
-        clickEndButton.setOnClickListener(clickEndListener);
-
+               clickEndButton.setOnClickListener(clickEndListener);
     }
 
     private final View.OnClickListener clickStartListener = new View.OnClickListener() {
@@ -52,15 +50,14 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-
     private final View.OnClickListener clickEndListener = new View.OnClickListener() {
         @SuppressLint("SetTextI18n")
         @Override
         public void onClick(View v) {
 
             Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            messagePhoto.setText("Фото:  " + number);
-            startActivity(intent);
+                 messagePhoto.setText("Фото:  " + number);
+                    startActivity(intent);
         }
     };
 }
